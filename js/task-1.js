8fcf06a3a262886d5d4ff408280e49e600146dba
Viktoriia Shavranska <viktoriashavranska@gmail.com>
2025-12-14 14:22:52 +0100

    changes in code

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids, you have ${
-    credits - totalPrice
-  } credits left`;
+  return 'You ordered {quantity} droids worth {totalPrice} credits!';
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
